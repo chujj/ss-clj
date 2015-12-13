3f481d0f60937d35b627ce1d59c8ffb61382f79e
jiajun.zhu <jiajun.zhu@upai.com>
2015-12-13 13:26:36 +0800

    init version

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ss-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ss_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns ss-clj.core-test
+  (:require [clojure.test :refer :all]
+            [ss-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
