ae598d9f1cb241163506a9bd4be1638574e6c206
jiajun.zhu <jiajun.zhu@upai.com>
2015-12-16 19:37:53 +0800

    test parse commandline

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot ss-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ss_clj/core.clj
+++ src/ss_clj/core.clj
@@ -3,7 +3,8 @@
   (:use (ss-clj utils))
   (:require [ss-clj.socks5_server :as s5server]
             [ss-clj.local_server :as lserver]
-            [ss-clj.remote_server :as rserver])
+            [ss-clj.remote_server :as rserver]
+            [clojure.tools.cli :as cli])
   (:import (java.net ServerSocket Socket InetSocketAddress UnknownHostException)
            (java.io BufferedReader InputStreamReader InputStream ByteArrayInputStream PrintWriter)))
 
@@ -41,8 +42,73 @@
   (doto (Thread. #(socket-server println-socket 8009)) (.start))
   (socket-server local-server 8008))
 
+
+(def cli-options
+  ;; An option with a required argument
+  [["-m" "--run-mode MODE" "MODEs: socks5-proxy rserver lserver"
+    :default "socks5-proxy"
+    :parse-fn #(str %)
+    :validate [#(or (= % "socks5-proxy") (= % "lserver") (= % "rserver")) "Must one of: socks5-proxy rserver lserver"]]
+   [nil "--rserver-addr RServer-ADDR" "only work when mode==lserver"
+    :default "127.0.0.1"
+    ]
+   [nil "--rserver-port RServer-Port" "only work when mode==lserver"
+    :default 8009
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]
+    ]
+   [nil "--crypto-method Method" "MODEs: plain aes-cbc-128"
+    :default "plain"
+    :parse-fn #(str %)
+    :validate [#(or (= % "plain") (= % "aes-cbc-128")) "Must one of: plain aes-cbc-128"]]
+   [nil "--password PASSWORD" "password, now it must 16-bytes"
+    :default "0123456789123456"
+    :parse-fn #(str %)
+    :validate [#(= (count %) 16) "Must 16-bytes"]]
+   ["-p" "--port PORT" "Port number"
+    :default 8008
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ;; A non-idempotent option
+   ["-v" nil "Verbosity level"
+    :id :verbosity
+    :default 0
+    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn setup-crypto-method
+  [a b]
+  (println a "  ---   " b))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-                                        ;  (socket-server socks5-proxy-server 8008)
-  (ss-proxy))
+  (let [argument (cli/parse-opts args cli-options)
+        mode (get-in argument [:options :run-mode])
+        port (get-in argument [:options :port])]
+    (cond
+      (:errors argument) ; error
+      (println (:errors argument))
+          
+      (get-in argument [:options :help])
+      (println (:summary argument)) ; help
+
+      (= mode "dev-test")
+      (do (println mode)
+          (ss-proxy))
+
+      (= mode "socks5-proxy")                            ; socks5-proxy
+      (do (println mode) (socket-server socks5-proxy-server port))
+
+      (= mode "rserver")                ;rserver
+      (do (println mode)
+          (setup-crypto-method (get-in argument [:options :crypto-method]) (get-in argument [:options :password]))
+          (socket-server remote-server port))
+
+      (= mode "lserver")                ;lserver
+      (do (println mode)
+          (setup-crypto-method (get-in argument [:options :crypto-method]) (get-in argument [:options :password]))
+          (socket-server local-server port))
+          ))
+  )
